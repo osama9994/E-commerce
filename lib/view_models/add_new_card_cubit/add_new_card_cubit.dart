0a5534589639a2ecfd5bc7424a350ef5f4464a3b
osama9994 <alshalbeosama6@gmail.com>
2025-10-25 13:36:11 +0300

    mastercard foto problem done

--- lib/view_models/add_new_card_cubit/add_new_card_cubit.dart
+++ lib/view_models/add_new_card_cubit/add_new_card_cubit.dart
@@ -1,30 +1,75 @@
+
+
 import 'package:animation_project/models/payment_cart_model.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 
 part 'add_new_card_state.dart';
 
 class AddNewCardCubit extends Cubit<AddNewCardState> {
   AddNewCardCubit() : super(AddNewCardInitial());
 
-void addNewCard(String cardNumber, String cardHolderName,String expiryDate,String cvv){
-  emit(AddNewCardLoading());
-PaymentCartModel newCard=PaymentCartModel(
-  id: DateTime.now().toIso8601String(),
-  cardNumber: cardNumber,
-  cardHolderName: cardHolderName,
-  expiryDate: expiryDate,
-  cvv: cvv,
-  );
-  Future.delayed(
-   const Duration(seconds: 1),(){
+  Future<void> addNewCard(
+    String cardNumber,
+    String cardHolderName,
+    String expiryDate,
+    String cvv,
+  ) async {
+    try {
+      emit(AddNewCardLoading());
+
+      // إنشاء الكرت الجديد
+      final newCard = PaymentCartModel(
+        id: DateTime.now().toIso8601String(),
+        cardNumber: cardNumber,
+        cardHolderName: cardHolderName,
+        expiryDate: expiryDate,
+        cvv: cvv,
+      );
+
+      // تأخير بسيط لمحاكاة عملية حفظ أو استدعاء API
+      await Future.delayed(const Duration(seconds: 1));
+
+      // إضافة البطاقة إلى القائمة التجريبية
       dummyPaymentCards.add(newCard);
+
+      // نجاح العملية
       emit(AddNewCardSuccess());
+    } catch (e) {
+      // في حال وجود خطأ
+      emit(AddNewCardFailure(errorMessage: e.toString()));
     }
-  );
-dummyPaymentCards.add(newCard);
-  emit(AddNewCardSuccess());
+  }
 }
 
-}
+
+
+// import 'package:animation_project/models/payment_cart_model.dart';
+
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+// part 'add_new_card_state.dart';
+
+// class AddNewCardCubit extends Cubit<AddNewCardState> {
+//   AddNewCardCubit() : super(AddNewCardInitial());
+
+// void addNewCard(String cardNumber, String cardHolderName,String expiryDate,String cvv){
+//   emit(AddNewCardLoading());
+// PaymentCartModel newCard=PaymentCartModel(
+//   id: DateTime.now().toIso8601String(),
+//   cardNumber: cardNumber,
+//   cardHolderName: cardHolderName,
+//   expiryDate: expiryDate,
+//   cvv: cvv,
+//   );
+//   Future.delayed(
+//    const Duration(seconds: 1),(){
+//       dummyPaymentCards.add(newCard);
+//       emit(AddNewCardSuccess());
+//     }
+//   );
+// dummyPaymentCards.add(newCard);
+//   emit(AddNewCardSuccess());
+// }
+
+// }
--- lib/views/widgets/payment_method_item.dart
+++ lib/views/widgets/payment_method_item.dart
@@ -2,34 +2,35 @@ import 'package:animation_project/models/payment_cart_model.dart';
 import 'package:animation_project/utils/app_color.dart';
 import 'package:flutter/material.dart';
 
-
 class PaymentMethodItem extends StatelessWidget {
-  const PaymentMethodItem({super.key, required this.paymentCard, required this.onItemTapped});
-final PaymentCartModel paymentCard;
-final VoidCallback onItemTapped;
+  const PaymentMethodItem({
+    super.key,
+    required this.paymentCard,
+    required this.onItemTapped,
+  });
+  final PaymentCartModel paymentCard;
+  final VoidCallback onItemTapped;
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onItemTapped,
-      child: DecoratedBox(decoration: 
-      BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: AppColor.white,
-        border: Border.all(
-          color:AppColor.grey
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: AppColor.white,
+          border: Border.all(color: AppColor.grey),
         ),
-      ),
-      child:ListTile(
-        leading: Image.asset("mastercard_logo",
-        width: 50,
-        height: 50,
-        fit: BoxFit.contain,
-        
+        child: ListTile(
+          leading: Image.asset(
+            "images/mastercard_logo.png",
+            width: 50,
+            height: 50,
+            fit: BoxFit.contain,
+          ),
+          title: Text("MasterCard"),
+          subtitle: Text(paymentCard.cardNumber),
+          trailing: Icon(Icons.chevron_right),
         ),
-        title:  Text("MasterCard"),
-        subtitle: Text(paymentCard.cardNumber),
-        trailing: Icon(Icons.chevron_right),
-      ) ,
       ),
     );
   }
